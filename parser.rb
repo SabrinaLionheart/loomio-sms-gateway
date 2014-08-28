4d2f176abdaf6c860c8504173fd642f0dc36bcc7
Severin <GazerDwarf@Gmail.com>
2014-08-28 21:41:25 +1200

    Continued the parser, still no testing.

--- parser.rb
+++ parser.rb
@@ -7,11 +7,31 @@ end
 class Parser
 	def initialise
 		# Build a hash to relate each command name to it's class
-		commands = {}
+		@commands = {}
 		Command.commands.each do |command|
-			commands[command.name.upcase] = command
+			@commands[command.name.upcase] = command
 		end
 		# A hash for partial commands
 		unfinished = {}
+	end
+	def parse(message)
+		# If a command is awaiting additional input
+		if @unfinished.has_key? message.num
+			command = @unfinished[message.num]
+			command.append message
+			if command.ready?
+				command.run
+				@commands.delete message.num
+			end
+		# Otherwise make a new command
+		else
+			command = @commands[message.split.first.upcase]
+			command = command.new message
+			if command.ready?
+				command.run
+			else
+				@unfinished[message.num] = command
+			end
+		end
 	end
 end
