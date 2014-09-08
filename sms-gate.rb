2776c68165b54b126a2ef1e9d3e8f375801bde42
Severin <GazerDwarf@Gmail.com>
2014-09-08 13:12:35 +1200

    sms-gate now closes connections correctly so the phone doesn't die

--- sms-gate.rb
+++ sms-gate.rb
@@ -11,12 +11,14 @@ loop do
 	puts "Listening..."
 	client = smsSocket.accept
 	message = client.recvfrom 1000
+	client.puts "K tnx"
+	client.close
 	message = message.first.split
 	message = message[1]
 	message = message[2..-1]
 	message = CGI.parse message
 	message = Message.new message["phone"].first, message["text"].first
 	puts message.inspect
-	parser.parse message
+	parser.parse message if message.num && message.msg
 end
 
