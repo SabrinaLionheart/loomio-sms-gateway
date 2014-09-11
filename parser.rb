9de920b48cc697e68a5eb10c8b8419d4c94a8d9c
Severin <GazerDwarf@Gmail.com>
2014-09-11 14:18:12 +1200

    The parser should handle failing commands now.

--- parser.rb
+++ parser.rb
@@ -15,35 +15,47 @@ class Parser
 		@unfinished = {}
 	end
 	def parse(message)
-		# If a command is awaiting additional input
-		if @unfinished.has_key? message.num
-			# Get command
-			command = @unfinished[message.num]
-			command.ammend message
-			if command.ready?
-				command.run
-				# The command was used remove it
-				@unfinished.delete message.num
-			end
-			# Perhaps the command wanted to say something
-			return command.response	
-		# Otherwise make a new command
-		else
-			command = @commands[message.msg.split.first.upcase]
-			# Check the command exists
-			unless command
-				puts "Bad command"
-				puts message.inspect
-				# Error message would go here
-				return nil
-			end
-			command = command.new message
-			if command.ready?
-				command.run
+		# Be prepared for a fatal error in commands
+		begin
+			# If a command is awaiting additional input
+			if @unfinished.has_key? message.num
+				# Get command
+				command = @unfinished[message.num]
+				command.ammend message
+				if command.ready?
+					command.run
+					# The command was used remove it
+					@unfinished.delete message.num
+				end
+				# Perhaps the command wanted to say something
+				return command.response	
+			# Otherwise make a new command
 			else
-				@unfinished[message.num] = command
+				command = @commands[message.msg.split.first.upcase]
+				# Check the command exists
+				unless command
+					puts "Bad command"
+					puts message.inspect
+					# Error message would go here
+					return nil
+				end
+				command = command.new message
+				if command.ready?
+					command.run
+				else
+					@unfinished[message.num] = command
+				end
+				return command.response
 			end
-			return command.response
+		rescue Exception => e
+			# The command encountered a fatal error
+			# Spam information to the terminal
+			puts e.message
+			puts e.backtrace.inspect
+			# Remove bad command
+			@unfinished.delete message.num
+			# Tell the user about the failure
+			return Message.new message.num, "Sorry, there was a fatal error while processing your command."
 		end
 	end
 end
