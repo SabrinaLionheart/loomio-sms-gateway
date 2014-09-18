1e8cf8474e9656e36fd8290462e47c1334fe0d95
jeffmomo <jeff941027@gmail.com>
2014-09-18 14:20:17 +1200

    Update GetSummary.rb

--- commands/GetSummary.rb
+++ commands/GetSummary.rb
@@ -36,7 +36,7 @@ class GetSummary < Command
 		user = DummyAPI.getUserByNumber(message.num)
 	
 		# sends back response from API
-		@response = Message.new @num, DummyAPI.getSummary user
+		@response = Message.new(@num, DummyAPI.getSummary(user))
 	end
 
 	def process(message)
