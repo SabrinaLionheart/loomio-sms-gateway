a0d34c6660334d0c21996e601d87142e4e0ef3bb
Severin <GazerDwarf@Gmail.com>
2014-09-24 17:05:34 +1200

    Made the gate way less gross. Preparing to move recieving and sending into separate threads.

--- sms-gate.rb
+++ sms-gate.rb
@@ -1,62 +1,41 @@
-# This would be replaced to use a different gateway
-# This is for the SMS Gateway Android app
-# This should never be deployed because it's bad, so is the phone.
 require_relative 'message'
 require_relative 'parser'
-require 'cgi'
 require 'net/http'
-require 'socket'
-require 'uri'
+require 'sinatra'
 
-if ARGV.size != 2
+# Arguments
+if ARGV.size !=2
 	puts "Bad arguments!\nruby sms-gate.rb <server address> <password>"
 	exit -1
 end
-smsServer = ARGV[0]
+server = ARGV[0]
 pass = ARGV[1]
-# Socket to recieve get requests
-smsSocket = TCPServer.new "0.0.0.0", 8080
-# Parser to parse messages
+
+# New parser with ~5 minutes of timeout on messages
 parser = Parser.new 5
-# Unhelpful message
-puts "I'm alive"
-loop do
-	puts "Listening..."
-	# Accept connection
-	client = smsSocket.accept
-	# Read message
-	message = client.recvfrom 1000
-	# Respond to client and close connection
-	client.puts "K tnx"
-	client.close
-	# Parse get request
-	message = message.first.split
-	message = message[1]
-	message = message[2..-1]
-	message = CGI.parse message
-	message = Message.new message["phone"].first, message["text"].first
-	# Debug:
-	puts message.inspect
-	# Parse message
-	reply = parser.parse message if message.num && message.msg
-	# Send response if present
+
+# Tell sinatra to use port 8080
+set :port, 8080
+
+# Tell sinatra to do the work
+get '/' do
+	# Make sure the required parameters are present
+	# Report when they are missing
+	return "Need parameter phone" unless params[:phone]
+	return "Need parameter text" unless params[:text]
+
+	# Process the message
+	reply = parser.parse Message.new params[:phone], params[:text]
+
+	# Send the reply
 	if reply
-		begin
-			# Make almost correct get request, escape it and 
-			# store it as a URI
-			url = URI.parse URI.escape("http://#{smsServer}:9090/sendsms?phone=#{reply.num}&text=#{reply.msg}&password=#{pass}")
-			# Make the URI a get request proper
-			req = Net::HTTP::Get.new url.to_s
-			# Do the request
-			res = Net::HTTP.start url.host, url.port do |http|
-				http.request req
-			end
-			# Profit
-			puts "Sent #{reply.inspect}"
-		rescue
-			# Do not profit
-			puts "Phone is misbehaving again..."
-		end
+		# Build the url
+		url = URI.parse URI.escape("http://#{server}:9090/sendsms?phone=#{reply.num}&text=#{reply.msg}&password=#{pass}")
+		# Attempt to send message
+		resp = Net::HTTP.get_response(url)
+		# Sucess
+		return resp.body if resp.is_a?(Net::HTTPSuccess)
+		# Else failure
+		return "Failed to send message!"
 	end
 end
-
