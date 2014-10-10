b1e54353438c34c98b6f7748d98b72ea598acf5c
Aflah Bhari <aflahmohamed@gmail.com>
2014-10-10 18:06:03 +1300

    Removed old SubscribeTo command Refined the new one Added a GetPoll command Added a MessageHelper class

--- commands/subscribeTo.rb
+++ commands/subscribeTo.rb
@@ -1,21 +1,16 @@
 class Subscribe < Command
-	@name = "subscribe"
+##
+# Makes a command to subscribe a user to a subdomain
+# subscribe <subdomain>
+def self.subscribe(message)
+message.msg.slice! 0..name.size
+group = message.split " "
 
-	##
-	# Makes a command to subscribe a user to a subdomain
-	#
-	# Needs API functions that add users to a 
-	def subscribe(message)
-		@group	= subdomain
-		@user	= username
-		if(api.Sub(user) == success)
-			msg = "You have been subscribed to the group " + group
-			# The message
-			Message.new msg
-		else
-			msg = “Sorry, that group does not exist”
-			# The message
-			Message.new msg
-	end
-	
+msg = "You have been subscribed to the group " + group
+# The message
+return Message.new message.num if api.Subscribe
+msg = “Sorry, that group does not exist”
+# The message
+return Message.new message.num api.Subscribe
+end
 end
