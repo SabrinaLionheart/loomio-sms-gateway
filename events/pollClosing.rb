b40387e3d4b43b6021e343881a8be9ac0b24dc29
Severin <GazerDwarf@Gmail.com>
2014-10-13 14:02:28 +1300

    Poll closing soon now works.

--- events/pollClosing.rb
+++ events/pollClosing.rb
@@ -1,28 +1,28 @@
 ##
-# A new discussion has been created and the user is subscribed to it
+# A poll is closing and the user is subscribed to it
 #
 class PollClosing < Event
-	@name = "pollClosing"
+	@name = "motion_closing_soon"
 
 	##
 	# Creates the message to notify the user of the event
 	#
 	def self.handle(event)
-	
-			# Making a call to the API giving it a proposal number and getting a proposal 
-		proposal = LoomioAPI.getProposal propNum
+		proposal = event["motion"]
+		subscription = event["subscription"]
+		hoursRemaining = event["hours_remaining"]
 
 		percentAgree	=	MessageHelper.percentage proposal["yes_votes_count"], proposal["votes_count"]		
 		percentDisagree =	MessageHelper.percentage proposal["no_votes_count"], proposal["votes_count"]
 		percentAbstain	=	MessageHelper.percentage proposal["abstain_votes_count"], proposal["votes_count"]
 		percentBlock	=	MessageHelper.percentage proposal["block_votes_count"], proposal["votes_count"]
 	
-		msg = "Proposal #{event["proposal name"]} is about to close. The current positions are:
-		Agree		=	#{percentAgree}
-		Disagree	=	#{percentDisagree}
-		Abstain		=	#{percentAbstain}
-		Block		=	#{percentBlock}"
+		msg = "The proposal #{proposal["name"]} is closing in #{hoursRemaining} hours. The current positions are:
+Agree		=	#{percentAgree}
+Disagree	=	#{percentDisagree}
+Abstain		=	#{percentAbstain}
+Block		=	#{percentBlock}"
 		# The message
-		Message.new event["number"], msg
+		Message.new subscription["tag"], msg
 	end
 end
