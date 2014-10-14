6c3832cad27b0cbc86401393ff46d07c3065d05d
jeffmo <jeff941027@gmail.com>
2014-10-15 01:39:57 +1300

    Updated with new APIs

--- loomioAPI.rb
+++ loomioAPI.rb
@@ -20,11 +20,13 @@ class LoomioAPI
 
     # Loads from config the API URL.
     @APICommands = YAML.load_file "apiConfig.yaml"
-    raise "loomio url not correctly defined!" unless @APICommands[:apiurl]
+    raise "loomio url not correctly defined!" unless @APICommands[:apiPath]
     raise "gate api url not correctly defined!" unless @APICommands[:handler_url]
 
 
-    @url = @APICommands[:apiurl]
+    @uri = URI.parse(@APICommands[:apiurl])
+    @http = Net::HTTP.new @uri.host, @uri.port
+    @path = @APICommands[:apiPath]
     @handlerURL = @APICommands[:handler_url]
 
     $stderr.puts "API initialised"
@@ -43,25 +45,38 @@ class LoomioAPI
   #
   private def getResponse(apiURL, apiParams, params = nil)
 
-    uri = URI(@url + apiURL + apiParams)
-    uri.query = URI.encode_www_form(params) if params
+            req = Net::HTTP::Get.new(@path + apiURL + '/' + apiParams)
+            req.set_form_data(params) unless params == nil
 
-    $stderr.puts "sending request to #{uri.to_s}"
+            $stderr.puts "sending GET to #{@uri.to_s + @path + apiURL}"
 
-    return Net::HTTP.get_response(uri)
+            return @http.request(req)
 
-  end
+          end
 
 
 
   # posts data in hash to the api url
-  def postRequest(apiURL, data)
+  def postRequest(apiURL, apiParams = "", data)
 
-    uri = URI(@url + apiURL)
+    req = Net::HTTP::Post.new(@path + apiURL + '/' + apiParams)
+    req.set_form_data(data)
 
-    $stderr.puts "sending request to #{uri.to_s} with data #{data.to_s}"
+    $stderr.puts "sending POST to #{@uri.to_s + @path + apiURL} with data #{data.to_s}"
 
-    return Net::HTTP.post_form(uri, data)
+    return @http.request(req)
+
+
+  end
+
+  def deleteRequest(apiURL, apiParams, data)
+
+    req = Net::HTTP::Delete.new(@path + apiURL + '/' + apiParams)
+    req.set_form_data(data)
+
+    $stderr.puts "sending DELETE to #{@uri.to_s + @path + apiURL} with data #{data.to_s}"
+
+    return @http.request(req)
 
 
   end
@@ -97,56 +112,14 @@ class LoomioAPI
         return User.new number
         
     end
-    
-    
-    ##
-    # use loomio api to get the list of groups the user is involved in
-    # Returns nothing if no groups
-    #
-  def getUserGroups(user)
-        
-        # returns dummy list
-        return ["Team Aqua", "Team Magma", "Team Rocket", "Team xX420BL4Z3Xx", "Test Group"]
-        
-    end
-    
-    
-    
-    ##
-    # Gets info about the given group in the loomio database relative to that user.
-    #
-  def getGroupStatus(user)
-    	
-    	# sample output
-    	return 'public'
-    	
-    	# return 'private'
-    	
-    	# return 'invalid'
-    end
-    
-    ##
-    # use loomio api to get the list of groups the user is involved in
-    # Returns nothing if no groups
-    #
-  def getSubscribedGroups(user)
-        
-        # returns dummy list
-        return ["Team Aqua", "Team Magma"]
-        
-    end
-    
-    ##
-    # Returns an array of ongoing discussions in the group
-    #
 
 
-    # Gets an array of proposals in JSON format when given a subdomain name. The first member of the array is status code of whether the request was successful
+  # Gets an array of proposals in JSON format when given a subdomain name. The first member of the array is status code of whether the request was successful
   #
   #####################################################################################################################################################################################################################################################################
   def getProposalsBySubdomain(subdomain)
 
-    return jsonfy getResponse("active_proposals/", subdomain)
+    return jsonfy getResponse("/active_proposals", subdomain)
   end
 
   #
@@ -154,7 +127,7 @@ class LoomioAPI
   #
   def getLatestProposalByKey(key)
 
-    return jsonfy getResponse("proposals/", key)
+    return jsonfy getResponse("/proposals", key)
 
   end
 
@@ -163,118 +136,43 @@ class LoomioAPI
   #
   def subscribeToSubdomain(subdomain, number)
 
-    return jsonfy postRequest("api_group_subscriptions", {:subdomain => subdomain, :tag => number, :path => @handlerURL})
+    return jsonfy postRequest("/api_group_subscriptions", {:subdomain => subdomain, :tag => number, :path => @handlerURL})
 
   end
 
+
+  def unsubscribeFromSubdomain(subdomain, number)
+
+    return jsonfy deleteRequest("/api_group_subscriptions/", subdomain, {:tag => number, :path => @handlerURL})
+
+  end
 
   #####################################################################################################################################################################################################################################################################
 
-
-    ##
-    # use loomio api to unsubscribe the user from the given group
-    # 
-    #
-  def unsubscribeFromGroup(user, group)
-        
-        # returns result of API call or success
-        return 'success'
-        
-    end
-    
-    ##
-    # use loomio api to subscribe the user to the given group
-    #
-  def subscribeToGroup(user, group)
-        
-        # returns result of API call or success
-        return 'success'
-        
-  end
-
-
-    
-    
-    ##
-    # use loomio api to get stuff the user has recently been involved in.
-    #
-  def getUserSummary(user)
-        
-        # Returns a sample summary
-        return "Nothing happened in any of the groups you care about. You can charge your iPhone with a microwave now though, pretty neat."
-
-  end
-
-
-  # Gets the proposal summary by key
-  def getProposalSummary(key)
-
-    arr = getLatestProposalByKey(key)
-
-    if arr[0] == "200" # if key exists
-      hash = arr[1]
-
-      # returns array in the order of yes,no,abstain,block
-      return [hash["yes_votes_count"], hash["no_votes_count"], hash["abstain_votes_count"], hash["block_votes_count"]]
-    end
-
-    return arr[0]
-
-
-  end
-
-  # Gets the latest proposals of the given subdomain
-  def getGroupDiscussions(group)
-
-
-    hash = getProposalsBySubdomain(group)
-
-
-
-    if (status = hash.shift) == "200"
-      output = Array.new
-
-      hash.each do |x|
-        output << x["name"]
-      end
-
-      return output
-    end
-
-    return status
-
-    #return ["World Domination", "Funniest Cat Picture", "An Interesting Discussion"]
-
-  end
-
-
-  ##
-    # use loomio api to get stuff the user has recently been involved in.
-    #
-  def getPollSummary(poll)
-        
-        # Returns a sample summary
-        return %Q(The <#{poll}> positions are
-Agree		=	<50%>
-Disagree	=	<80%>
-Abstain		=	<10%>
-Block		=	<3%>)
-    end
 
 end
 
 #testing purposes
 def test
 
-  #puts LoomioAPI.api.subscribeToSubdomain("abstainers", "656565").to_s
+  begin
 
-  #puts LoomioAPI.api.getGroupDiscussions("abstainers").to_s
+    puts LoomioAPI.api.subscribeToSubdomain("abstainers", "656565").to_s
 
-  #puts LoomioAPI.api.getProposalSummary("xEtj48Rz").to_s
+    puts LoomioAPI.api.getProposalsBySubdomain("abstainers").to_s
 
-  # puts LoomioAPI.api.getProposalsBySubdomain("abstainers").to_s
+    puts LoomioAPI.api.getLatestProposalByKey("xEtj48Rz").to_s
 
-  #puts LoomioAPI.api.getLatestProposalByKey("xEtj48Rz").to_s
+    puts LoomioAPI.api.unsubscribeFromSubdomain("abstainers", "656565").to_s
+
+  rescue Exception
+
+    $stderr.puts "TESTING FAIELD"
+
+  end
+
+
+
 
 end
 
