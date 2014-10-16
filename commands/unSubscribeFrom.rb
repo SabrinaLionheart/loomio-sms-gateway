482c3b421501722a5138118e9708c6ca3333443e
Sam Roseweir <samroseweir@gmail.com>
2014-10-16 15:55:10 +1300

    Add unSubscribeFrom.rb

---
+++ commands/unSubscribeFrom.rb
@@ -0,0 +1,23 @@
+##
+# Will be used to unsubscibe a user from a sub domain
+class ViewGroup < Command
+	##
+	# Processes a message for unSubscribeFrom
+	# unSubscribeFrom <subdomain>
+	def self.process(message)
+		# Remove the command name from the message
+		message.msg.slice! 0..name.size
+		arguments = message.msg.split " "
+		subdomain = arguments.first
+
+		# An API call unsubscribing the user from a group
+		status, response = LoomioAPI.unsubscribeFromSubdomain subdomain, message.num
+
+		# Return this message if the group the user asked for doesn't exist
+		return Message.new message.num, "That group does not exist" unless status == 200
+
+		# Return successful message
+		return Message.new message.num, "You have been unsubscribed from the group #{subdomain}"
+
+	end
+end
